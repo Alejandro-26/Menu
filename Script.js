1bb8d5a6fe6a05453970b56da2d1884a40fb6f3d
Alejo Duran <111656887+Alejandro-26@users.noreply.github.com>
2023-11-29 20:09:04 -0500

    Subiendo proyecto de Menu con efectos elegantes.

---
+++ Script.js
@@ -0,0 +1,18 @@
+const menutoggle = document.querySelector(".menutoggle");
+const navigation = document.querySelector(".navigation");
+
+menutoggle.onclick = function(){
+    navigation.classList.toggle('open');
+}
+
+const list = document.querySelectorAll('.list');
+
+function activarLink() {
+    list.forEach((item) =>
+    item.classList.remove('active'))
+    this.classList.add('active')
+}
+
+list.forEach((item)=>
+item.addEventListener('click', activarLink)
+)
